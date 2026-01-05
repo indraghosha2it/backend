1e4fdbdc27f73a3c6361f6c027433f66739955d3
indraghosha2it <indraghosh.a2it@gmail.com>
2026-01-05 13:21:26 +0600

    modified backend

--- server.js
+++ server.js
@@ -1,76 +1,9 @@
-// const express = require('express');
-// const cors = require('cors');
-// // require('dotenv').config();
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
-// const PORT = process.env.PORT || 5000;
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-
-// // user name: indraghosha2it_db_user
-// // password: lK9eXloK1ehvs32A
-
-
-
-
-// // mongoDB CODE
-
-// const uri = "mongodb+srv://indraghosha2it_db_user:3ZtFZcmwF4pqay9Z@cluster0.2xrkmju.mongodb.net/?appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-// // Basic route
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Cost Analysis API is running' });
-// });
-
-
-
-// // Routes will be added here
-// // app.use('/api/expenses', require('./routes/expenseRoutes'));
-// // app.use('/api/auth', require('./routes/authRoutes'));
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ error: 'Something went wrong!' });
-// });
-
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
